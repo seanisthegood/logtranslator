8821b162d0a4da3b88d7e61d66bc21f08fa942ce
seanisthegood <seanmhart@gmail.com>
2024-06-07 14:04:02 -0400

    request redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify,request# type: ignore
+from flask import Flask, render_template, request, jsonify,redirect# type: ignore
 import openai
 from dotenv import load_dotenv
 import os
